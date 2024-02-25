26823c5faf145d6e9cbd54cc210ec0b8a9609f5b
Iwakura-dev <escayhokage@gmail.com>
2024-02-24 16:04:59 -0800

    feat: 🎸 add base
    base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: __dirname,
+  base: "/react-test-task-valantis/",
   plugins: [react()],
 });
